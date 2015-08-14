d5996a1bc2593f33cda0aac25bb6a2373bcd2e5a
realmaster42 <totalrobloxian@hotmail.com>
2015-08-14 22:42:15 +0000

    0.1.0 Snapshot C
    Item placed on block breaking fix,
    Magenta,Violet and Purple colored stones.

---
+++ 1.8/MC Forge 1.8 Paint Mod/Minecraft/src/main/java/com/mp_Poder/lib/RefStrings.java
@@ -0,0 +1,10 @@
+package com.mp_Poder.lib;
+
+public class RefStrings {
+
+	 public static final String MODID = "pmod2015";
+	 public static final String NAME = "Paint Mod";
+	 public static final String VERSION = "0.1.0";
+	 public static final String CLIENTSIDE = "com.mp_Poder.paintmod.ClientProxy";
+	 public static final String SERVERSIDE = "com.mp_Poder.paintmod.ServerProxy";
+}
